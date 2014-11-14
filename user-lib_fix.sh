386147318af40295f0fcec5d2d2288bc01573fc6
Ascully24 <anthony.scully24@gmail.com>
2014-11-14 15:52:25 +0000

    Added the fix libs script.

---
+++ user-lib_fix.sh
@@ -0,0 +1,18 @@
+
+FILES="/usr/lib/*"
+
+for f in $FILES
+do
+
+        ext=$(echo $f |awk -F . '{if (NF>1) {print $NF}}')
+
+        if [ "$ext" != "la" ]
+        then
+                #echo "Processing $f"
+                fname=$(basename $f)
+                #fname="$f" | cut -d'.' --complement -f2-
+                fname=$(echo $fname |awk -F . '{if (NF=1) {print $NF}}')
+                echo $fname
+        fi
+
+done
